823b673364ea2fe35fb750e932d35db93ed6e93d
Allu101 <44586813+Allu101@users.noreply.github.com>
2023-01-08 21:18:45 +0200

    Replace index.js with correct code

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-
-const electricityRouter = require('./routes/electricity');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use('/api/electricity', electricityRouter);
+const app = require('./app');
 
 const PORT = 5000;
 
