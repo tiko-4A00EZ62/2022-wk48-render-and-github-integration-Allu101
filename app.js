b563474c5c587032d2531413e1204b8af19e6bc5
Allu101 <44586813+Allu101@users.noreply.github.com>
2023-01-08 21:18:15 +0200

    Replace app.js with correct code

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const PORT = 5000;
+const electricityRouter = require('./routes/electricity');
 
-app.listen(PORT, () => {
-  console.log(`Backend is listening on PORT ${PORT}`);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/health', (req, res) => {
+  res.send('OK');
 });
+
+
+app.use('/api/electricity', electricityRouter);
+
+module.exports = app;
